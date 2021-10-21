f150bd60a156654119250fa9ff79ca995a75c4e6
Мария <87368091+MariaKorkina@users.noreply.github.com>
2021-10-21 18:21:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -12,10 +12,24 @@ async function getResponse () {
     })
   }
   console.log(newArr);
+  let indexDel1 = getRandomIntInclusive(0, newArr.length-1)
+  newArr.splice(indexDel1,1);
+ console.log(indexDel1);
+ 
+  let indexDel2 = getRandomIntInclusive(0, newArr.length-1)
+  newArr.splice(indexDel2,1);
+  console.log(indexDel2);
+  console.log(newArr);
+  
   // delete newArr[(Math.floor(Math.random()*newArr.length)]
   // console.log(newArr);
 }
 
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
+}
 
-getResponse()
+ getResponse()
 
